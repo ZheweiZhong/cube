c704d108d6f15e6d1ee3347c2eb37b996f9e50ae
ZheweiZhong <22671256@student.uwa.edu.au>
2022-04-17 10:31:39 +0800

    1

--- rev/dashboard-app/src/pages/DashboardPage.js
+++ rev/dashboard-app/src/pages/DashboardPage.js
@@ -4,222 +4,131 @@ import Typography from '@material-ui/core/Typography';
 import ChartRenderer from '../components/ChartRenderer';
 import Dashboard from '../components/Dashboard';
 import DashboardItem from '../components/DashboardItem';
-const DashboardItems = [
-  // {
-  //   id: 1,
-  //   name: 'kWh Charging and Maintianing Charge by Day',
-  //   vizState: {
-  //     query: {
-  //       measures: ['WeekCharge.energy', 'WeekMaintain.energy'],
-  //       timeDimensions: [
-  //         {
-  //           dimension: 'WeekCharge.starttime',
-  //           dateRange: ['2012-07-01', '2012-07-31'],
-  //         },
-  //       ],
-  //       order: [['SequenceOfWeek.id', 'asc']],
-  //       limit: 5000,
-  //       filters: [],
-  //       dimensions: ['WeekCharge.dayOfWeek', 'SequenceOfWeek.id'],
-  //     },
-  //     chartType: 'bar',
-  //   },
-  // },
-  // {
-  //   id: 2,
-  //   name: 'kWh Charging and Maintianing Charge by Hour',
-  //   vizState: {
-  //     query: {
-  //       measures: ['DayCharge.energy', 'DayMaintain.energy'],
-  //       timeDimensions: [
-  //         {
-  //           dimension: 'DayCharge.starttime',
-  //           dateRange: ['2012-07-01', '2012-07-31'],
-  //         },
-  //       ],
-  //       order: [['DayCharge.hourOfDay', 'asc']],
-  //       limit: 5000,
-  //       filters: [],
-  //       dimensions: ['DayCharge.hourOfDay'],
-  //     },
-  //     chartType: 'bar',
-  //   },
-  // },
-  {
-    id: 1,
-    name: 'Energy Supply by Stations',
-    vizState: {
-      query: {
-        measures: ['StationChargesAc1.kWh'],
-        timeDimensions: [
-          {
-            dimension: 'StationChargesAc1.starttime',
-            dateRange: ['2012-07-01', '2012-07-31'],
-          },
-        ],
-        order: {
-          'StationChargesAc1.kwh': 'desc',
-        },
-        limit: 5000,
-        filters: [],
-        dimensions: ['ChargingStation.organisation'],
-      },
-      chartType: 'pie',
-    },
-  }, 
-  {
-    id: 2,
-    name: 'Period Energy',
-    vizState: {
-      query: {
-        measures: ['PeriodEnergy.energy'],
-        timeDimensions: [
-          {
-            dimension: 'PeriodEnergy.starttime',
-            dateRange: ['2012-07-01', '2012-07-31'],
-          },
-        ],
-        order: {
-          'PeriodEnergy.energy': 'desc',
-        },
-        dimensions: ['PeriodEnergy.period'],
-      },
-      chartType: 'pie',
-    },
-  },
-  // {
-  //   id: 4,
-  //   name: 'Hours Charging and Maintianing Charge by Hour',
-  //   vizState: {
-  //     query: {
-  //       measures: ['WeekMaintainHour.spent', 'WeekChargeHour.spent'],
-  //       timeDimensions: [
-  //         {
-  //           dimension: 'WeekMaintainHour.starttime',
-  //           dateRange: ['2012-07-01', '2012-07-31'],
-  //         },
-  //       ],
-  //       order: [['SequenceOfWeek1.id', 'asc']],
-  //       limit: 5000,
-  //       filters: [],
-  //       dimensions: ['WeekChargeHour.dayOfWeek', 'SequenceOfWeek1.id'],
-  //     },
-  //     chartType: 'bar',
-  //   },
-  // },
-  // {
-  //   id: 2,
-  //   name: 'Period Energy',
-  //   vizState: {
-  //     query: {
-  //       measures: ['PeriodKwh.energy'],
-  //       timeDimensions: [
-  //         {
-  //           dimension: 'PeriodKwh.starttime',
-  //           dateRange: ['2012-07-01', '2012-07-31'],
-  //         },
-  //       ],
-  //       order: {
-  //         'PeriodKwh.energy': 'desc',
-  //       },
-  //       dimensions: ['PeriodKwh.period'],
-  //     },
-  //     chartType: 'pie',
-  //   },
-  // }, // {
-  //   id: 6,
-  //   name: 'Hours Charging and Maintianing Charge by Day',
-  //   vizState: {
-  //     query: {
-  //       measures: ['DayChargeHour.spent', 'DayMaintainHour.spent'],
-  //       timeDimensions: [
-  //         {
-  //           dimension: 'DayChargeHour.starttime',
-  //           dateRange: ['2012-07-01', '2012-07-31'],
-  //         },
-  //       ],
-  //       order: [['DayChargeHour.hourOfDay', 'asc']],
-  //       dimensions: ['DayChargeHour.hourOfDay'],
-  //     },
-  //     chartType: 'bar',
-  //   },
-  // },
-  {
-    id: 3,
-    name: 'kWh Charging and Maintianing Charge by Day ',
-    vizState: {
-      query: {
-        measures: ['WeekCharge.energy', 'WeekMaintain.energy'],
-        timeDimensions: [
-          {
-            dimension: 'WeekCharge.starttime',
-            dateRange: ['2012-07-01', '2012-07-31'],
-          },
-        ],
-        order: [['WeekCharge.day_number', 'asc']],
-        filters: [],
-        dimensions: ['WeekCharge.day_of_week', 'WeekCharge.day_number'],
-      },
-      chartType: 'bar',
-    },
-  },
-  {
-    id: 4,
-    name: 'Hours Charging and Maintianing Charge by Hour',
-    vizState: {
-      query: {
-        measures: ['WeekChargeHour.hours', 'WeekMaintainHour.hours'],
-        timeDimensions: [
-          {
-            dimension: 'WeekChargeHour.starttime',
-            dateRange: ['2012-07-01', '2012-07-31'],
-          },
-        ],
-        order: [['WeekChargeHour.day_number', 'asc']],
-        dimensions: ['WeekChargeHour.day_of_week', 'WeekChargeHour.day_number'],
-      },
-      chartType: 'bar',
-    },
-  },
-  {
-    id: 5,
-    name: 'kWh Charging and Maintianing Charge by Hour',
-    vizState: {
-      query: {
-        measures: ['DayCharge.energy', 'DayMaintain.energy'],
-        timeDimensions: [
-          {
-            dimension: 'DayCharge.starttime',
-            dateRange: ['2012-07-01', '2012-07-31'],
-          },
-        ],
-        order: [['DayCharge.hour', 'asc']],
-        dimensions: ['DayCharge.hour'],
-      },
-      chartType: 'bar',
-    },
-  },
-  {
-    id: 6,
-    name: 'kWh Charging and Maintianing Charge by Day',
-    vizState: {
-      query: {
-        measures: ['DayChargeHour.spent', 'DayMaintainHour.spent'],
-        timeDimensions: [
-          {
-            dimension: 'DayChargeHour.starttime',
-            dateRange: ['2012-07-01', '2012-07-31'],
-          },
-        ],
-        order: [['DayChargeHour.hour_on', 'asc']],
-        dimensions: ['DayChargeHour.hour_on'],
-      },
-      chartType: 'bar',
-    },
-  },
-];
+import Toolbar from "../components/Toolbar.js";
+
+
+
 
 const DashboardPage = () => {
+  const [startDate, setStartDate] = React.useState(new Date("2012-07-01T00:00:00"));
+  const [finishDate, setFinishDate] = React.useState(new Date("2012-07-31T00:00:00"));
+  const DashboardItems = [
+    {
+      id: 1,
+      name: 'Energy Supply by Stations',
+      vizState: {
+        query: {
+          measures: ['StationChargesAc1.kWh'],
+          timeDimensions: [
+            {
+              dimension: 'StationChargesAc1.starttime',
+              dateRange: [startDate, finishDate],
+            },
+          ],
+          order: {
+            'StationChargesAc1.kwh': 'desc',
+          },
+          limit: 5000,
+          filters: [],
+          dimensions: ['ChargingStation.organisation'],
+        },
+        chartType: 'pie',
+      },
+    }, 
+    {
+      id: 2,
+      name: 'Period Energy',
+      vizState: {
+        query: {
+          measures: ['PeriodEnergy.energy'],
+          timeDimensions: [
+            {
+              dimension: 'PeriodEnergy.starttime',
+              dateRange: [startDate, finishDate],
+            },
+          ],
+          order: {
+            'PeriodEnergy.energy': 'desc',
+          },
+          dimensions: ['PeriodEnergy.period'],
+        },
+        chartType: 'pie',
+      },
+    },
+    {
+      id: 3,
+      name: 'kWh Charging and Maintianing Charge by Day ',
+      vizState: {
+        query: {
+          measures: ['WeekCharge.energy', 'WeekMaintain.energy'],
+          timeDimensions: [
+            {
+              dimension: 'WeekCharge.starttime',
+              dateRange: [startDate, finishDate],
+            },
+          ],
+          order: [['WeekCharge.day_number', 'asc']],
+          filters: [],
+          dimensions: ['WeekCharge.day_of_week', 'WeekCharge.day_number'],
+        },
+        chartType: 'bar',
+      },
+    },
+    {
+      id: 4,
+      name: 'Hours Charging and Maintianing Charge by Hour',
+      vizState: {
+        query: {
+          measures: ['WeekChargeHour.hours', 'WeekMaintainHour.hours'],
+          timeDimensions: [
+            {
+              dimension: 'WeekChargeHour.starttime',
+              dateRange: [startDate, finishDate],
+            },
+          ],
+          order: [['WeekChargeHour.day_number', 'asc']],
+          dimensions: ['WeekChargeHour.day_of_week', 'WeekChargeHour.day_number'],
+        },
+        chartType: 'bar',
+      },
+    },
+    {
+      id: 5,
+      name: 'kWh Charging and Maintianing Charge by Hour',
+      vizState: {
+        query: {
+          measures: ['DayCharge.energy', 'DayMaintain.energy'],
+          timeDimensions: [
+            {
+              dimension: 'DayCharge.starttime',
+              dateRange: [startDate, finishDate],
+            },
+          ],
+          order: [['DayCharge.hour', 'asc']],
+          dimensions: ['DayCharge.hour'],
+        },
+        chartType: 'bar',
+      },
+    },
+    {
+      id: 6,
+      name: 'kWh Charging and Maintianing Charge by Day',
+      vizState: {
+        query: {
+          measures: ['DayChargeHour.spent', 'DayMaintainHour.spent'],
+          timeDimensions: [
+            {
+              dimension: 'DayChargeHour.starttime',
+              dateRange: [startDate, finishDate],
+            },
+          ],
+          order: [['DayChargeHour.hour_on', 'asc']],
+          dimensions: ['DayChargeHour.hour_on'],
+        },
+        chartType: 'bar',
+      },
+    },
+  ];
   const dashboardItem = (item) => (
     <Grid item xs={12} lg={6} key={item.id}>
       <DashboardItem title={item.name}>
@@ -242,8 +151,18 @@ const DashboardPage = () => {
   );
 
   return DashboardItems.length ? (
-    <Dashboard>{DashboardItems.map(dashboardItem)}</Dashboard>
-  ) : (
+    <div>
+      <Toolbar
+       startDate={startDate}
+       setStartDate={setStartDate}
+       finishDate={finishDate}
+       setFinishDate={setFinishDate}
+      />
+      <Dashboard>
+        {DashboardItems.map(dashboardItem)}
+      </Dashboard>
+    </div>
+    ) : (
     <Empty />
   );
 };
